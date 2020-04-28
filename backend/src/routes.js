466f14460db1fdf2e4f03d5823e6e6b744b29116
danspiegel <danispiegel@gmail.com>
2020-04-28 01:37:44 -0300

    Adicionada validação dos valores nas rotas de acesso aos Controllers.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
+
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
@@ -10,23 +12,57 @@ const routes = express.Router();
 * Rotas do OngController
 */
 routes.get('/ongs', OngController.index);
-routes.post('/ongs', OngController.create);
+
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.number().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2)
+    })
+}), OngController.create);
 
 /**
  * Rotas do IncidentController
  */
-routes.get('/incidents', IncidentController.index);
-routes.post('/incidents', IncidentController.create);
-routes.delete('/incidents/:id', IncidentController.delete);
+routes.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number()
+    })
+}), IncidentController.index);
+
+routes.post('/incidents', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        value: Joi.number().required(),
+        ong_id: Joi.string().required()
+    })
+}), IncidentController.create);
+
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required()
+    })
+}), IncidentController.delete);
 
 /**
  * Rotas do ProfileController
  */
-routes.get('/profile', ProfileController.index);
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required()
+    }).unknown()
+}), ProfileController.index);
 
 /**
  * Rotas do SessionController
  */
-routes.post('/sessions', SessionController.create);
+routes.post('/sessions', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        id: Joi.string().required()
+    })
+}), SessionController.create);
 
 module.exports = routes;
